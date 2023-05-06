362d69e7081c53d3d7593af28261279d31506990
martinjurkovic <martin.jurkovic19@gmail.com>
2023-05-06 10:28:43 +0200

    Add split script for mutagenesis dataset

---
+++ src/split_mutagenesis.py
@@ -0,0 +1,28 @@
+# %%
+import rike.split_utils as utils
+# %%
+# READ DATA
+dataset_name = "mutagenesis"
+original_data = utils.read_tables(dataset_name)
+
+# %%
+# SPLIT DATASET 10 FOLD
+
+molecule_folds = utils.split_k_fold(original_data["molecule"])
+
+# %%
+atom_folds = utils.split_k_fold_on_parent(
+    molecule_folds, original_data["atom"], [("molecule_id", "molecule_id")])
+# len(atom_folds[0][0])
+
+# %%
+bond_folds = utils.split_k_fold_on_parent(atom_folds, original_data["bond"],
+                                          [("atom1_id", "atom_id"), ("atom2_id", "atom_id")])
+# len(bond_folds[0][1])
+
+# %%
+utils.save_folds(
+    [molecule_folds, atom_folds, bond_folds],
+    dataset_name,
+    ["molecule", "atom", "bond"])
+# %%
