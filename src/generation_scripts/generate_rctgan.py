980fb1604a26fc38c0a9e9b983c10c362e47ede2
ValterH <valter.hudovernik@gmail.com>
2023-05-19 20:38:13 +0200

    update rctgan generation

--- src/generation_scripts/generate_rctgan.py
+++ src/generation_scripts/generate_rctgan.py
@@ -1,4 +1,5 @@
 # %%
+import os
 import argparse
 import pickle
 
@@ -11,15 +12,30 @@ from rctgan import RCTGAN
 # %%
 
 args = argparse.ArgumentParser()
-args.add_argument("--dataset_name", type=str, default="rossmann-store-sales")
+args.add_argument("--dataset-name", type=str, default="rossmann-store-sales")
 args = args.parse_args()
 
 dataset_name = args.dataset_name
+
+CWD_PROJECT = os.getcwd().split(
+    'Synthetic-data-generation-project')[0] + 'Synthetic-data-generation-project'
+base_path = CWD_PROJECT + '/data/synthetic/' + dataset_name + '/RCTGAN/'
 
 # GENERATE SYNTHETIC DATA
 for k in tqdm.tqdm(range(10)):
     tables_train, tables_test = generation_utils.get_train_test_split(dataset_name, k)
     metadata = sdv_metadata.generate_metadata(dataset_name, tables_train)
+    
+    # check if data already exists
+    generated = False
+    for table_name in metadata.to_dict()['tables'].keys():
+        path = base_path + f'{dataset_name}_{table_name}_fold_{k}.csv'
+        if os.path.exists(path):
+            generated = True
+            break
+    if generated:
+        continue
+
     model = RCTGAN(metadata)
     # ignores warnings being raised inside the RCTGAN package
     with pd.option_context('mode.chained_assignment', None):
