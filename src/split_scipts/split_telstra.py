31aefb72eeee92899458759abe057654b3bda5ad
martinjurkovic <martin.jurkovic19@gmail.com>
2023-05-19 19:33:21 +0200

    Add ids to telstra split

--- src/split_scipts/split_telstra.py
+++ src/split_scipts/split_telstra.py
@@ -7,6 +7,12 @@ DATASET_NAME = "telstra-competition-dataset"
 original_data = utils.read_tables(DATASET_NAME)
 
 # %%
+original_data['severity_type'] = utils.add_primary_key(original_data['severity_type'], 'severity_type_id')
+original_data['resource_type'] = utils.add_primary_key(original_data['resource_type'], 'resource_type_id')
+original_data['log_feature'] = utils.add_primary_key(original_data['log_feature'], 'log_feature_id')
+original_data['event_type'] = utils.add_primary_key(original_data['event_type'], 'event_type_id')
+
+# %%
 # SPLIT DATASET 10 FOLD
 train_folds = utils.split_k_fold(original_data["train"])
 # %%
