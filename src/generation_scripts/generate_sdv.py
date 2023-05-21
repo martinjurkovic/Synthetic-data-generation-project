98ffc5834e008f0a5b8f9024dadd7982d0942610
ValterH <valter.hudovernik@gmail.com>
2023-05-21 18:36:02 +0200

    update sdv generation

--- src/generation_scripts/generate_sdv.py
+++ src/generation_scripts/generate_sdv.py
@@ -1,4 +1,6 @@
 # %%
+import os
+import pickle
 import argparse
 from rike import utils
 from rike.generation import sdv_metadata
@@ -10,15 +12,18 @@ logger = logging_config.logger
 
 args = argparse.ArgumentParser()
 args.add_argument("--dataset-name", type=str, default="rossmann-store-sales")
+args.add_argument("--limit", type=int, default=10)
 args = args.parse_args()
 
 dataset_name = args.dataset_name
+limit = args.limit
 root_table_name = sdv_metadata.get_root_table(dataset_name)
 
 # %%
 # GENERATE SYNTHETIC DATA
-limit = 5
 for k in tqdm.tqdm(range(limit)):
+    model_save_path = f'models/sdv/{dataset_name}/model_{k}.pickle'
+    os.makedirs(os.path.dirname(model_save_path), exist_ok=True)
     logger.warning("Generating synthetic data for %s, fold %d", dataset_name, k)
     tables_train, tables_test = utils.get_train_test_split(dataset_name, k, limit=limit)
     metadata = sdv_metadata.generate_metadata(dataset_name, tables_train)
@@ -26,6 +31,8 @@ for k in tqdm.tqdm(range(limit)):
     logger.warning("Fitting HMA1 model...")
     model = HMA1(metadata=metadata)
     model.fit(tables_train)
+    with open(model_save_path, "wb") as f:
+        pickle.dump(model, open(model_save_path, "wb" ) )
     logger.warning("Done!")
     logger.warning("Sampling synthetic data...")
     synthetic_data = model.sample(num_rows=tables_test[root_table_name].shape[0])
