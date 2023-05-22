9991389b5b1d83f0d0c324f559448a8adb70d9c7
martinjurkovic <martin.jurkovic19@gmail.com>
2023-05-22 18:29:41 +0200

    Fix unzip_mostlyai

--- src/download_scripts/unzip_mostlyai_data.py
+++ src/download_scripts/unzip_mostlyai_data.py
@@ -1,13 +1,20 @@
 import os
 import zipfile
+import argparse
 
 import pandas as pd
+# add args
+args = argparse.ArgumentParser()
+args.add_argument("--dataset-name", type=str, default="rossmann-store-sales")
+dataset_name = args.dataset_name
 
 base_path = 'data/downloads/mostlyai'
 zip_fies = os.listdir(base_path)
 for file in zip_fies:
     if file.endswith(".zip"):
-        dataset = file.split('_fold')[0]
+        dataset = file.split('_')[0].lower()
+        if dataset not in dataset_name.lower():
+              continue
         unzip_path = os.path.join(base_path, dataset)
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(f'{base_path}/{file}', 'r') as zip_ref:
@@ -18,6 +25,8 @@ for file in zip_fies:
                for file in files:
                     file_path = os.path.join(unzip_path, subfolder, file)
                     new_path = os.path.join('data/synthetic', dataset, 'mostlyai', f'{dataset}_{file}')
+                    # create new folder if it does not exist
+                    os.makedirs(os.path.dirname(new_path), exist_ok=True)
                     df = pd.read_csv(file_path)
                     print(file_path)
                     if 'bond' in file:
